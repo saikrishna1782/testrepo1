837fec7c249d656dd77e55dfaccfc75b542d088c
saikrishna1782 <saikrishnak.0705@gmail.com>
2025-05-12 16:22:28 -0700

    adding App.vue

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+<div>
+  <!-- data binding using double curly braces-->
+    <h1>{{greet}} {{name}}</h1>
+
+  <!-- data binding using v-text directive. Drawback of v-text is it replaces entire html element.-->
+    <h1 v-text="channel"></h1>
+
+
+</div>
+
+</template>
+
+<script>
+
+
+export default {
+  name: 'App',
+  //using data() function to initialize properties: think of ot like a constructor for a class
+  data()
+  {
+    return {
+      greet:"Hello",
+      name:"binding text using double curly braces",
+      channel:"binding text using v-text directive",
+    };
+  }
+  
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
+<!-- this is rleated to main branch-->
